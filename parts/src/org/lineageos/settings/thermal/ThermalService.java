b5a624950b1d2d8f6e3970f27ddc09ec0ba2b63f
Nauval Rizky <enuma.alrizky@gmail.com>
2021-12-09 23:53:29 +0700

    lisa: Parts: Use performance thermal as  fallback for GameSpace's listed apps
    This will apply the "performance" thermal preset to the app that listed
    at GameSpace's library, but aren't yet configured by user on thermal
    settings.
    With this behavior, we can get saner default on gaming scenarios without
    having to force user choice.
    
    - We don't have performance config in thermal, so replace it with gaming.
    - Also add benchmark, browser and streaming in TPF
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- parts/src/org/lineageos/settings/thermal/ThermalService.java
+++ parts/src/org/lineageos/settings/thermal/ThermalService.java
@@ -27,12 +27,16 @@ import android.content.IntentFilter;
 import android.content.res.Configuration;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.provider.Settings;
 import android.util.Log;
+
+import java.util.Arrays;
 
 public class ThermalService extends Service {
 
     private static final String TAG = "ThermalService";
     private static final boolean DEBUG = false;
+    private static final String SETTINGS_GAME_LIST = "gamespace_game_list";
 
     private String mPreviousApp;
     private ThermalUtils mThermalUtils;
@@ -77,6 +81,23 @@ public class ThermalService extends Service {
         this.registerReceiver(mIntentReceiver, filter);
     }
 
+    private boolean isListedOnGameSpace(String packageName) {
+        String[] gameList = Settings.System.getString(getContentResolver(),
+                SETTINGS_GAME_LIST).split(";");
+        if (packageName == null || gameList.length == 0) {
+            return false;
+        }
+
+        return Arrays.stream(gameList).map(data -> {
+            String[] userGame = data.split("=");
+            return userGame.length == 2 ? userGame[0] : data;
+        }).anyMatch(it -> it.equals(packageName));
+    }
+
+    private boolean isConfigured(String packageName) {
+        return mThermalUtils.getStateForPackage(packageName) != ThermalUtils.STATE_DEFAULT;
+    }
+
     private final TaskStackListener mTaskListener = new TaskStackListener() {
         @Override
         public void onTaskStackChanged() {
@@ -87,7 +108,11 @@ public class ThermalService extends Service {
                     ComponentName taskComponentName = focusedTask.topActivity;
                     String foregroundApp = taskComponentName.getPackageName();
                     if (!foregroundApp.equals(mPreviousApp)) {
+                        if (!isConfigured(foregroundApp) && isListedOnGameSpace(foregroundApp)) {
+                        mThermalUtils.setThermalProfileForce(ThermalUtils.STATE_GAMING);
+                    } else {
                         mThermalUtils.setThermalProfile(foregroundApp);
+					}
                         mPreviousApp = foregroundApp;
                     }
                 }
