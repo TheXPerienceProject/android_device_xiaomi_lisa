a14179fc051cf533875b2cffe06e6fe65370eafb
AdarshGrewal <adarshgrewal@gmail.com>
2022-06-02 21:43:37 +0530

    lisa: Stop building drm vendor hidl
    * was only requested by wfdhdcphalservice

--- device.mk
+++ device.mk
@@ -209,8 +209,7 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
-    android.hardware.drm@1.4.vendor
+    android.hardware.drm@1.4-service.clearkey
 
 # Fastbootd
 PRODUCT_PACKAGES += \
