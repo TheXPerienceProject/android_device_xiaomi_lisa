dce4683d49cd9876965181cecaedbd2e8dc055e6
klozz <carlosj@klozz.dev>
2022-06-19 12:06:07 +0200

    lisa: implement libinit to define all variants
    Signed-off-by: klozz <carlosj@klozz.dev>

--- xperience_lisa.mk
+++ xperience_lisa.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2022 PixelOS
+# Copyright (C) 2022 The XPerience Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lisa device
 $(call inherit-product, device/xiaomi/lisa/device.mk)
 
-# Inherit some common PixelOS stuff
+# Inherit some common XPerience stuff
 $(call inherit-product, vendor/xperience/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 #TARGET_GAPPS_ARCH := arm64
@@ -21,6 +22,13 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := lisa
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_NAME := xperience_lisa
-PRODUCT_MODEL := Xiaomi 11 Lite NE
+PRODUCT_MODEL := Xiaomi 11 Lite 5G NE
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys" \
+    PRODUCT_NAME=lisa_global \
+    PRODUCT_MODEL=2109119DG
+
+BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
