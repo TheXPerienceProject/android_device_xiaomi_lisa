4450f0f8a08a6007c9021c0fce5a212d12780121
klozz <carlosj@klozz.dev>
2022-07-08 13:42:42 +0200

    lisa: build kernel with yuki clang 14
    Signed-off-by: klozz <carlosj@klozz.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,6 +121,7 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := \
     LLVM_IAS=1 \
     TARGET_PRODUCT=$(PRODUCT_DEVICE)
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/yuki-clang
 TARGET_KERNEL_SOURCE := kernel/xiaomi/lisa
 TARGET_KERNEL_CONFIG := lisa_defconfig
 
