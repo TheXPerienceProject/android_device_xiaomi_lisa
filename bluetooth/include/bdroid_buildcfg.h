1954be8e5b444da6c886a2b049ded0975472c4e8
klozz <carlosj@klozz.dev>
2022-09-03 02:06:52 +0200

    lisa: track bluetooth device name from marketname
    Signed-off-by: klozz <carlosj@klozz.dev>

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -6,6 +6,7 @@
  *
  * Copyright (C) 2012 The Android Open Source Project
  * Copyright (C) 2018 The LineageOS Project
+ * Copyright (C) 2022 The XPerience Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +24,45 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
+#include <stdint.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int property_get(const char *key, char *value, const char *default_value);
+#ifdef __cplusplus
+}
+#endif
+
+#include "osi/include/osi.h"
+
+typedef struct {
+	const char *product_device;
+	const char *product_model;
+} device_t;
+
+static const device_t devices[] = {
+};
+
+static inline const char *BtmGetDefaultName()
+{
+	char product_device[92];
+	property_get("ro.product.marketname", product_device, "");
+
+	for (unsigned int i = 0; i < ARRAY_SIZE(devices); i++) {
+		device_t device = devices[i];
+
+		if (strcmp(device.product_device, product_device) == 0) {
+			return device.product_model;
+		}
+	}
+
+	//return fallback to ro.product.model
+	return "";
+}
+
+#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    32
