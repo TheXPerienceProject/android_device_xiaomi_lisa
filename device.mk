af045c9e830263713c7d590b0c0c07181cf343ff
Alexander Winkowski <dereference23@outlook.com>
2022-09-04 04:42:37 +0000

    lisa: Disable UBWC for WFD
    As it turns out, yupik does not support UBWC:
    [drm:sde_encoder_virt_atomic_check:1028] [sde error]enc43 mode unsupported, phys idx 0
    [drm:sde_encoder_phys_wb_atomic_check:769] [sde error]invalid output format 3231564e
    
    Change-Id: If445caf152428abb3dce74091dac9b4c9d2e26d5
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -664,3 +664,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml
+
+# WiFi Display
+PRODUCT_SYSTEM_PROPERTIES += \
+    vendor.sys.video.disable.ubwc=1
