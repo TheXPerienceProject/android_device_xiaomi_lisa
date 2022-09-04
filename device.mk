fc873952cf39395f76f40cd67e810a55550d0c1c
klozz <carlosj@klozz.dev>
2022-09-04 03:49:38 +0200

    lisa: build hidl memory vendor variant explicitly
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -283,6 +283,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
+    android.hidl.memory@1.0 \
+    android.hidl.memory@1.0.vendor \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
