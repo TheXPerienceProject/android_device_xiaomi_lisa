d78c7ad66e124102e873087809e5b55afcfac796
klozz <carlosj@klozz.dev>
2022-09-20 14:40:14 +0200

    lisa: switch android.hardware.bluetooth to 1.0
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -104,7 +104,7 @@ PRODUCT_PACKAGES += \
     libldacBT_bco.vendor
 
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.1.vendor \
+    android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
