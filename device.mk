9bad3999de0cc541906ef336a4b1ed9bcdd9ede6
klozz <carlosj@klozz.dev>
2022-09-27 19:59:08 +0200

    lisa: move to android.hardware.bluetooth.audio-impl
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -103,10 +103,12 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     libbthost_if \
     libbthost_if.vendor \
+    libbthost_if_sink.vendor \
     libldacBT_bco.vendor
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth.audio-impl \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
