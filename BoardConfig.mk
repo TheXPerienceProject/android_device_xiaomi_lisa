bc2e213f1a034875df1e49bae379450f98e61238
klozz <carlosj@klozz.dev>
2022-10-05 12:21:32 +0200

    Revert "lisa: enforcing!"
     - Bluetooth is crashing due to bad selinux_android_setcontext
    
       10-04 22:00:55.030  2721  2721 E SELinux : seapp_context_lookup:  No match for app with uid 1002, seinfo default, name com.android.bluetooth
       10-04 22:00:55.030  2721  2721 E SELinux : selinux_android_setcontext:  Error setting context for app with uid 1002, seinfo default:privapp:targetSdkVersion=33:complete: Success
       10-04 22:00:55.030  1884  2482 D CompatibilityChangeReporter: Compat change id reported: 218533173; UID 10133; state: ENABLED
       10-04 22:00:55.030  2721  2721 F zygote64: jni_internal.cc:825] JNI FatalError called: (com.android.bluetooth) frameworks/base/core/jni/com_android_internal_os_Zygote.cpp:1748: selinux_android_setcontext(1002, 0, "default:privapp:targetSdkVersion=33:complete", "com.android.bluetooth") failed
    
    This reverts commit 6a4d55500ff439536daa0cd03f026650f2289ecf.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,7 +133,7 @@ BOARD_KERNEL_CMDLINE += swiotlb=noforce
 BOARD_KERNEL_CMDLINE += pcie_ports=compat
 BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
-
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_RAMDISK_USE_LZ4 := true
 
 # Kernel modules
