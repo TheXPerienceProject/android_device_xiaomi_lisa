1f6526fca2d8125f31a5767fce047ddf3978edcd
klozz <carlosj@klozz.dev>
2022-10-24 13:10:33 +0200

    lisa: remove nxp nfc source from tree
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -527,8 +527,6 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    $(LOCAL_PATH)/nxp \
-    device/xiaomi/lisa/nxp/secure_element/snxxx \
     packages/modules/Bluetooth/android/app
 
 # Telephony
