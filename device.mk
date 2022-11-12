0bbd724e0817e6f8eb00af146fe3315823237dac
klozz <carlosj@klozz.dev>
2022-11-12 04:18:37 +0100

    Revert "lisa:touch: add IHighTouchPollingRate"
    This reverts commit 36bea581221e68c5c92ecaf386dcd0f9538251cb.
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -559,10 +559,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
-# Touch
-PRODUCT_PACKAGES += \
-    vendor.xperience.touch@1.0-service.lisa
-
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
