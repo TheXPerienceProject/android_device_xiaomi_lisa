a6f0105edb07e1e847a2842b3b64cdf079f9caff
klozz <carlosj@klozz.dev>
2023-02-01 19:25:26 -0600

    Revert "lisa: Include MIUI Camera"
    * isnt working for me is crashing a lot
    
    This reverts commit d41612b2796c4480645f81760a983b7737ac5408.

--- init/init_lisa.cpp
+++ init/init_lisa.cpp
@@ -65,8 +65,7 @@ void set_ro_build_prop(const string &source, const string &prop,
     property_override(prop_name.c_str(), value.c_str());
 }
 
-void set_device_props(const string model, const string name, const string marketname,
-        const string mod_device) {
+void set_device_props(const string model, const string name, const string marketname) {
     // list of partitions to override props
     string source_partitions[] = { "", "bootimage.", "odm.", "product.",
                                    "system.", "system_ext.", "vendor." };
@@ -76,8 +75,6 @@ void set_device_props(const string model, const string name, const string market
         set_ro_build_prop(source, "name", name);
         set_ro_build_prop(source, "marketname", marketname);
     }
-
-    property_override("ro.product.mod_device", mod_device.c_str());
 }
 
 void vendor_load_properties()
@@ -86,15 +83,15 @@ void vendor_load_properties()
     string region = GetProperty("ro.boot.hwc", "");
 
     if (region == "IN") { // India
-        set_device_props("2109119DI", "lisa_in", "Xiaomi 11 Lite NE", "lisa_in_global");
+        set_device_props("2109119DI", "lisa_in", "Xiaomi 11 Lite NE");
         property_override("bluetooth.device.default_name", "Xiaomi 11 Lite NE");
         property_override("vendor.usb.product_string", "Xiaomi 11 Lite NE");
     } else if (region == "CN") { // China
-        set_device_props("2107119DC", "lisa", "Mi 11 LE", "lisa");
+        set_device_props("2107119DC", "lisa", "Mi 11 LE");
         property_override("bluetooth.device.default_name", "Mi 11 LE");
         property_override("vendor.usb.product_string", "Mi 11 LE");
     } else { // Global
-        set_device_props("2109119DG", "lisa_global", "Xiaomi 11 Lite 5G NE", "lisa_global");
+        set_device_props("2109119DG", "lisa_global", "Xiaomi 11 Lite 5G NE");
         property_override("bluetooth.device.default_name", "Xiaomi 11 Lite 5G NE");
         property_override("vendor.usb.product_string", "Xiaomi 11 Lite 5G NE");
     }
