7db9830840a170f0e57f3625de0d79fa4023f0a0
klozz <carlosj@klozz.dev>
2023-02-04 18:36:09 -0600

    lisa:update security patch to jan V14.0.2.0.TKOMIXM
    Signed-off-by: klozz <carlosj@klozz.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -213,7 +213,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-10-01
+VENDOR_SECURITY_PATCH := 2023-01-05
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
