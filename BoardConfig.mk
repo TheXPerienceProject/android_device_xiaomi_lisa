34b18573fb8ccfcb4cefdd3b377676abab1715db
klozz <carlosj@klozz.dev>
2023-03-07 08:01:20 -0600

    [DNM]Lisa: enable Compressed F2FS
    Signed-off-by: klozz <carlosj@klozz.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,6 +184,9 @@ TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_COPY_OUT_PRODUCT := product
 
+# Compression
+PRODUCT_FS_COMPRESSION := 1
+
 # Platform
 TARGET_BOARD_PLATFORM := lahaina
 
