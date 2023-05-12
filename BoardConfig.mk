401c8e85e4071a3cba8e0ff054254b6bf28a831a
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-05-12 14:40:17 +0530

    lisa: Lower default screen density to 400 dpi
    Much saner size for those who aren't visually impaired.
    Moto also uses this on their >6inch 1080p devices.
    
    Change-Id: I901ca3143f16f9f1b8b5b7150597991dd6f60992
    Signed-off-by: klozz <carlosj@klozz.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,9 @@ include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 # Board
 TARGET_BOOTLOADER_BOARD_NAME := lisa
 
+# Display
+TARGET_SCREEN_DENSITY := 400
+
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/lisa_QGKI.config
 
@@ -29,6 +32,7 @@ BOARD_DTBOIMG_PARTITION_SIZE := 25165824
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 
 # Inherit MiuiCamera Makefile
 -include vendor/xiaomi/miuicamera-$(PRODUCT_DEVICE)/BoardConfig.mk
