dce9ae118f5307f44fb424d6414e9b9c574926a1
klozz <carlosj@klozz.dev>
2024-05-01 01:35:51 -0600

    lisa: build usb from common
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -541,6 +541,7 @@ TARGET_COMMON_QTI_COMPONENTS := \
     media \
     perf \
     telephony \
+    usb \
     wfd
 
 # Power
@@ -640,16 +641,8 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti \
     android.hardware.usb@1.3 \
     android.hardware.usb@1.3.vendor
-
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/qcom/opensource/usb/etc
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # Vibrator
 PRODUCT_PACKAGES += \
