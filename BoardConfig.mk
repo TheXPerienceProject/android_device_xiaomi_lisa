2bbfd3c85d7e8910fc5800e17bc79f93bbd6c1d1
klozz <carlosj@klozz.dev>
2024-05-01 00:18:00 -0600

    lisa: inherit vendor_framework_compatibility_matrix from vendor
    Signed-off-by: klozz <carlosj@klozz.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,7 +97,8 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/hidl/vendor_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/configs/hidl/xiaomi_framework_compatibility_matrix.xml \
-    $(DEVICE_PATH)/configs/hidl/dolby.xml
+    $(DEVICE_PATH)/configs/hidl/dolby.xml \
+    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
     $(DEVICE_PATH)/configs/hidl/manifest_lahaina.xml \
