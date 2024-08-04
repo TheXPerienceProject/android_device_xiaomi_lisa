aa2aef3e7e31d000e6d2911d764429ad2b5fdc6a
Chrisl7 <wandersonrodriguesf1@gmail.com>
2024-08-04 00:34:12 +0000

    lisa: props: Use lmk kill_timeout_ms value from CLO
    [1] - Qualcomm has its own adaptation, via ro.lmk.kill_timeout_ms_dup. In this case we will just use the same value
    
    Change-Id: I350a0bd2502fb3e42db606d51b513137bc9a8509
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -450,7 +450,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.lmk.force_inkernel_lmk=true \
     ro.lmk.filecache_min_kb=153600  \
-    ro.lmk.kill_timeout_ms=50 \
+    ro.lmk.kill_timeout_ms=100 \
     ro.lmk.stall_limit_critical=40 \
     ro.lmk.use_minfree_levels=true
 
