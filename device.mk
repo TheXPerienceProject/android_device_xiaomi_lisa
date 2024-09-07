d0d15319d5155759c44ac675d4ca3c0bc654c1cc
klozz <carlosj@klozz.dev>
2024-09-07 05:22:17 -0600

    lisa: add 32bit libstagefright_foundation
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -707,6 +707,7 @@ PRODUCT_PACKAGES += \
     libvndksupport.vendor
 
 PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v33/arm/arch-arm-armv7-a-neon/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefright_foundation-v33.so \
     prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v33.so
 
 # Verified boot
