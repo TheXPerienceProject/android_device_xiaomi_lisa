1e64eadefd07e40a8b9d69d7755811f47835808e
Kyuofox <kyuofox@gmail.com>
2024-09-10 10:27:25 +0800

    lisa: Allow gettid call for atfwd
    We never know what upstream is thinking:
    W ATFWD-daemon: libminijail[4756]: failed to get path of fd 3: No such file or directory
    W ATFWD-daemon: libminijail[4756]: allowing syscall: connect
    W ATFWD-daemon: libminijail[4756]: allowing syscall: fcntl
    W ATFWD-daemon: libminijail[4756]: allowing syscall: sendto
    W ATFWD-daemon: libminijail[4756]: allowing syscall: socket
    W ATFWD-daemon: libminijail[4756]: allowing syscall: writev
    W ATFWD-daemon: libminijail[4756]: logging seccomp filter failures
    E ATFWD-daemon: libminijail[4756]: blocked syscall: gettid
    
    Change-Id: I42e638f9f25395b37df57471fce68cc73e11e83f
    Signed-off-by: Kyuofox <kyuofox@gmail.com>
    
    Signed-off-by: klozz <carlosj@klozz.dev>

--- extract-files.sh
+++ extract-files.sh
@@ -117,6 +117,10 @@ function blob_fixup() {
         vendor/lib/libstagefright_soft_ac4dec.so | vendor/lib/libstagefright_soft_ddpdec.so | vendor/lib/libstagefrightdolby.so | vendor/lib64/libdlbdsservice.so | vendor/lib64/libstagefright_soft_ac4dec.so | vendor/lib64/libstagefright_soft_ddpdec.so | vendor/lib64/libstagefrightdolby.so)
             grep -q "libstagefright_foundation-v33.so" "${2}" || "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
             ;;
+        vendor/etc/seccomp_policy/atfwd@2.0.policy)
+            [ "$2" = "" ] && return 0
+            echo 'gettid: 1' >> ${2}
+            ;;
     esac
 }
 
