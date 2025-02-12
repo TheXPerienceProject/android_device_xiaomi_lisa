3866029fefd3b66646675fd2840a0f8044ae41a5
klozz <carlosj@klozz.dev>
2025-02-12 02:17:24 -0600

    lisa: sepol cleanups and move vendor manifest call from vendor/com-/core-utils
    Signed-off-by: klozz <carlosj@klozz.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,7 +100,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/hidl/vendor_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/configs/hidl/xiaomi_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/configs/hidl/dolby.xml \
-    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/xperience/interfaces/vendor_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/hidl/device_framework_matrix.xml
 
